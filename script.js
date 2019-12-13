f099099ed3ac4c025a4892df5371851bf67027bb
kryst1anb <48355396+kryst1anb@users.noreply.github.com>
2019-12-13 11:23:54 +0100

    WERSJA OSTATECZNA - NIE BRAĆ

--- script.js
+++ script.js
@@ -1,140 +1,111 @@
+window.onload = function() {
+    ruch(window.innerWidth, window.innerHeight);
+    odswiez();
 
-        window.onload = function() {
+    document.getElementById('rozdzielczosc').innerHTML = window.innerWidth + "px x " + window.innerHeight + "px";
+    css();
+}
 
-            ruch(window.innerWidth, window.innerHeight);
-            odswiez();
-
-            document.getElementById('rozdzielczosc').innerHTML = window.innerWidth + "px x " + window.innerHeight + "px";
-            css();
+function ruch(width, height) {
+    var obiekty = document.querySelectorAll('.mozaika');
+    var pozycjaX, pozycjaY, that;
+    function startRuchu(zdarzenie, That) {
+        pozycjaX = zdarzenie.pageX;
+        pozycjaY = zdarzenie.pageY;
+        that = That;
+    }
+    function startRuchuDotknieciem(zdarzenie) {
+        if (zdarzenie.changedTouches.length > 0){
+            startRuchu(zdarzenie.changedTouches[0], this);
         }
-
-        function ruch(width, height) {
-            var obs = document.querySelectorAll('.mozaika');
-            var x, y, that;
-            var zatrzymaj = function(e) {
-                e.stopPropagation();
-                e.preventDefault();
-            }
-
-            var startRuchu = function(e, That) {
-                x = e.pageX;
-                y = e.pageY;
-                that = That;
-                zatrzymaj(e);
-            }
-
-            var startRuchuDotknieciem = function(e) {
-                if (e.changedTouches.length > 0)
-                    startRuchu(e.changedTouches[0], this);
-            }
-
-            var startRuchuMyszka = function(e) {
-                window.addEventListener('mousemove', ruchMyszkaLubDotykiem, true);
-                window.addEventListener('mouseup', koniecRuchuMyszka, true);
-                startRuchu(e, this);
-            }
-
-            var ruchMyszkaLubDotykiem = function(e) {
-                var dx = e.pageX - x;
-                var dy = e.pageY - y;
-                x = e.pageX;
-                y = e.pageY;
-                that.style.top = (that.offsetTop + dy) + 'px';
-                that.style.left = (that.offsetLeft + dx) + 'px';
-                wyslijNaSerwer();
-            }
-
-            var koniecRuchuMyszka = function(e) {
-                window.removeEventListener('mouseup', koniecRuchuMyszka, true);
-                window.removeEventListener('mousemove', ruchMyszkaLubDotykiem, true);
-                zatrzymaj(e);
-            }
-
-            var ruchDotykiem = function(e) {
-                if (e.changedTouches.length > 0) {
-                    ruchMyszkaLubDotykiem(e.changedTouches[0]);
-                }
-                zatrzymaj(e);
-            }
-
-            var koniecRuchuDotknieciem = function(e) {
-                zatrzymaj(e);
-            };
-
-            for (var i = 0; i < obs.length; i++) {
-                var ob = obs[i];
-                ob.addEventListener('mousedown', startRuchuMyszka, true);
-                ob.addEventListener('touchstart', startRuchuDotknieciem, false);
-                ob.addEventListener('touchmove', ruchDotykiem, false);
-                ob.addEventListener('touchend', koniecRuchuDotknieciem, false);
-            }
+    }
+    function startRuchuMyszka(zdarzenie) {
+        window.addEventListener('mousemove', ruchMyszkaLubDotykiem, true);
+        window.addEventListener('mouseup', koniecRuchuMyszka, true);
+        startRuchu(zdarzenie, this);
+    }
+    function ruchMyszkaLubDotykiem(zdarzenie) {
+        var poprawionaPozycjaX = zdarzenie.pageX - pozycjaX;
+        var poprawionaPozycjaY = zdarzenie.pageY - pozycjaY;
+        pozycjaX = zdarzenie.pageX;
+        pozycjaY = zdarzenie.pageY;
+        that.style.top = (that.offsetTop + poprawionaPozycjaY) + 'px';
+        that.style.left = (that.offsetLeft + poprawionaPozycjaX) + 'px';
+        wyslijNaSerwer();
+    }
+    function koniecRuchuMyszka() {
+        window.removeEventListener('mouseup', koniecRuchuMyszka, true);
+        window.removeEventListener('mousemove', ruchMyszkaLubDotykiem, true);
+    }
+    function ruchDotykiem(zdarzenie) {
+        if (zdarzenie.changedTouches.length > 0) {
+            ruchMyszkaLubDotykiem(zdarzenie.changedTouches[0]);
         }
+    }
+    for (var i = 0; i < obiekty.length; i++) {
+        var obiekt = obiekty[i];
+        obiekt.addEventListener('mousedown', startRuchuMyszka, true);
+        obiekt.addEventListener('touchstart', startRuchuDotknieciem, false);
+        obiekt.addEventListener('touchmove', ruchDotykiem, false);
+    }
+}
 
-        function wyslijNaSerwer() {
-            var kolox_ = document.getElementById("kolo").style.top;
-            var koloy_ = document.getElementById("kolo").style.left;
-            var trojkatx_ = document.getElementById("trojkat").style.top;
-            var trojkaty_ = document.getElementById("trojkat").style.left;
-            var kwadratx_ = document.getElementById("kwadrat").style.top;
-            var kwadraty_ = document.getElementById("kwadrat").style.left;
+function wyslijNaSerwer() {
+    var request = new XMLHttpRequest();
+    request.open("POST", "server.php", true);
+    request.send(JSON.stringify({
+        polecenie: 3,
+        "kolox": document.getElementById("kolo").style.top,
+        "koloy": document.getElementById("kolo").style.left,
 
-            var log = JSON.stringify({
-                polecenie: "set",
-                "kolox": kolox_,
-                "koloy": koloy_,
-                "trojkatx": trojkatx_,
-                "trojkaty": trojkaty_,
-                "kwadratx": kwadratx_,
-                "kwadraty": kwadraty_
-            });
+        "trojkatx": document.getElementById("trojkat").style.top,
+        "trojkaty": document.getElementById("trojkat").style.left,
 
-            var request = new XMLHttpRequest();
-            request.open("POST", "server.php", true);
-            request.setRequestHeader("Content-Type", "application/json");
-            request.send(log);
+        "kwadratx": document.getElementById("kwadrat").style.top,
+        "kwadraty": document.getElementById("kwadrat").style.left,
+
+        "trapezoidx": document.getElementById("trapezoid").style.top,
+        "trapezoidy": document.getElementById("trapezoid").style.left
+    }));
+}
+
+function odswiez() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            document.getElementById("kolo").style.top = response.kolox;
+            document.getElementById("kolo").style.left = response.koloy;
+            
+            document.getElementById("trojkat").style.top = response.trojkatx;
+            document.getElementById("trojkat").style.left = response.trojkaty;
+            
+            document.getElementById("kwadrat").style.top = response.kwadratx;
+            document.getElementById("kwadrat").style.left = response.kwadraty;
+            
+            document.getElementById("trapezoid").style.top = response.trapezoidx;
+            document.getElementById("trapezoid").style.left = response.trapezoidy;
         }
+    }
+    request.open("POST", "server.php", true);
+    request.send(JSON.stringify({
+        polecenie: 2
+    }));
+}
 
-        function odswiez() {
-            var request = new XMLHttpRequest();
-            var log = JSON.stringify({
-                polecenie: "get"
-            });
-            request.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    var response = JSON.parse(this.responseText);
-
-                    document.getElementById("kolo").style.top = response.kolox;
-                    document.getElementById("kolo").style.left = response.koloy;
-
-                    document.getElementById("trojkat").style.top = response.trojkatx;
-                    document.getElementById("trojkat").style.left = response.trojkaty;
-
-                    document.getElementById("kwadrat").style.top = response.kwadratx;
-                    document.getElementById("kwadrat").style.left = response.kwadraty;
-                }
-            }
-            request.open("POST", "server.php", true);
-            request.setRequestHeader("Content-Type", "application/json");
-            request.send(log);
+function css() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = this.responseText;
+            console.log("Ustawiony CSS:" + response);
+            document.getElementById("pagestyle").setAttribute("href", response);
         }
-
-        function css() {
-            var request = new XMLHttpRequest();
-            var log = JSON.stringify({
-                polecenie: "css"
-            });
-
-            request.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    var response = this.responseText;
-                    console.log("Ustawiony CSS:" + response);
-                    document.getElementById("pagestyle").setAttribute("href", response);
-                }
-            }
-
-            request.open("POST", "server.php", true);
-            request.setRequestHeader("Content-Type", "application/json");
-            request.send(log);
-        }
-        var time = setInterval(odswiez, 2000);
+    }
+    request.open("POST", "server.php", true);
+    request.send(JSON.stringify({ 
+        polecenie: 1
+    }));
+}
+setInterval(odswiez, 700);
     
--- server.php
+++ server.php
@@ -6,30 +6,44 @@
     $daneJSON = json_decode($suroweDane,true);
 
     $polecenie = intval($daneJSON['polecenie']);
-        
-    if($daneJSON['polecenie']  == "set" ){
-        file_put_contents("dane",$suroweDane);
-    }else if($daneJSON['polecenie']  == "get" ){
-		$plik = fopen("dane", "r") or die("Bład odczytu pliku");
-        $odczytPlik = fread($plik, filesize("dane"));
-        fclose($plik);
-        echo $odczytPlik;
-    }else if($daneJSON['polecenie']  == "css" ){
-        $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
-        if (strpos($user_agent, 'Windows')){
-            echo 'styleWindows.css';
-        }
-        else if (strpos($user_agent, 'Android')){
-            echo 'styleAndroid.css';
-        }
-        else if(strpos($user_agent, 'Mac')){
-            echo 'styleIOS.css';
-        }
-         else{
-            echo 'BRAK';
+    if(isset($daneJSON['polecenie']))
+    {
+        $polecenie = intval($daneJSON['polecenie']);
+        switch($polecenie)
+        {
+            case 1: 
+                $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+                if (strpos($user_agent, 'Windows')){
+                    echo 'styleWindows.css';
+                }
+                else if (strpos($user_agent, 'Android')){
+                    echo 'styleAndroid.css';
+                }
+                else if(strpos($user_agent, 'Mac')){
+                    echo 'styleIOS.css';
+                }
+                 else{
+                    echo 'BRAK';
+                } 
+            break;
+
+            case 2: 
+                $plik = fopen("dane", "r") or die("Bład odczytu pliku");
+                $odczytPlik = fread($plik, filesize("dane"));
+                fclose($plik);
+                echo $odczytPlik; 
+            break;
+
+            case 3: 
+                file_put_contents("dane",$suroweDane);
+            break;
+            default: 
+                $wynik = array('status' => false, 'kod' => 3, 'wartosc' => 'Podane zostało złe polecenie');
         }
     }
+
     flock($f, LOCK_UN); 
     fclose($f);
 ?>
